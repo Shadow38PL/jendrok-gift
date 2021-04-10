733928ece13c45fcb873a87c06d98425cc28b494
Artur Mędrygał <medrygal.artur@gmail.com>
2021-04-10 13:13:40 +0200

    random generation fix

--- src/app.py
+++ src/app.py
@@ -16,33 +16,33 @@ WIDTH = 480
 quotes = [
     "dron.on... dron OFF hyhyHY",
     "Bartek NIE DZIAŁAAAAAAAAAAA",
-    "Chłopaki, ale mam ;pomysła na biznes",
-    "Jeszcze miesiąc i będziemy ;zarabiać",
+    "Chłopaki, ale mam \npomysła na biznes",
+    "Jeszcze miesiąc i będziemy \nzarabiać",
     "głupie i bez sensu",
-    "Naprawdę musimy zaplanować;dalszy rozwój Heseya",
-    "nigdy heseya nie będzie ;miała logo",
+    "Naprawdę musimy zaplanować\ndalszy rozwój Heseya",
+    "nigdy heseya nie będzie \nmiała logo",
     "Kolektyw programistyczny Heseya",
-    "teraz to się nazywa ;heseya ciągły rozwój",
-    "Zamykamy firmę, chcesz premie;za acata czy nie",
+    "teraz to się nazywa \nheseya ciągły rozwój",
+    "Zamykamy firmę, chcesz premie\nza acata czy nie",
     "to za tą kasę zamykamy heseya",
     "Zamykamy tą heseje w pizdu",
-    "dokańczamy ft i komiksy;zamykamy spółkę",
+    "dokańczamy ft i komiksy\nzamykamy spółkę",
     "a chuj mi w dupe",
-    "ale nintendo ;to jest pojebane z cenami",
-    "a pierdolne sobie ;skrypciki pojebane",
-    "bardziej pojebane ;niż rework bazy danych",
-    "chińskie bajki to ;zawsze pojebane xd",
+    "ale nintendo \nto jest pojebane z cenami",
+    "a pierdolne sobie \nskrypciki pojebane",
+    "bardziej pojebane \nniż rework bazy danych",
+    "chińskie bajki to \nzawsze pojebane xd",
     "Zamknijmy to wszystko w pizdu",
     "Kup puki jest kasa xd",
-    "że kasa z acata idzie ;na bierzące a ja chce moje 125zł",
+    "że kasa z acata idzie \nna bierzące a ja chce moje 125zł",
     "JESTEŚMY BOGACI",
-    "tak to jest jak ;backend siada do frontu",
-    "huj wam w dupe ;paweł was oszukał",
+    "tak to jest jak \nbackend siada do frontu",
+    "huj wam w dupe \npaweł was oszukał",
     "Paweł dzwonił że chce wrócić",
     "ale ten paweł to kozak",
-    "Idziesz do sądu mówisz, Paweł jest ;be, a Paweł grzecznie płaci",
+    "Idziesz do sądu mówisz, Paweł jest \nbe, a Paweł grzecznie płaci",
     "Dzwonię. Paweł nie odbiera",
-    "paweł dzwonił i coś tam;faflunił o 400tys dotacji",
+    "paweł dzwonił i coś tam\nfaflunił o 400tys dotacji",
     "paweł to jest pajac"
 ]
 
@@ -50,28 +50,31 @@ quotes = [
 def main():
     pygame.init()
     pygame.display.set_caption("Jendrok")
-    display = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+    display = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
 
     renderer = Renderer(display)
 
-    currentText = ""
+    currentText = [""]
 
     def close():
         pygame.quit()
         sys.exit(0)
 
-    def getquote():
-        newText = currentText
-        while currentText == newText:
-            newText = quotes[random.randint(0, quotes.__len__() - 1)]
-        return "\"" + newText + "\""
+    def getQuote():
+        return "\"" + quotes[random.randint(0, quotes.__len__() - 1)] + "\""
 
-    def showquote():
-        currentText = getquote()
-        splitted = currentText.split(";")
-        text.text = splitted[0]
-        text2.text = splitted[1] if len(splitted) > 1 else ""
+    def generateQuote():
+        newText = getQuote()
+        while currentText[0] == newText:
+            newText = getQuote()
+        return newText
+
+    def showQuote():
+        currentText[0] = generateQuote()
+        splittedText = currentText[0].split("\n")
+        text.text = splittedText[0]
+        text2.text = splittedText[1] if len(splittedText) > 1 else ""
 
 
     title1 = Widget(Vector2Float(220, 40), Vector2Float(320, 32), None, None, "Jendrok MOOD", None)
@@ -82,10 +85,10 @@ def main():
     decor = Widget(Vector2Float(0, 280), Vector2Float(480, 40), Color(143, 2, 44), None, None, None)
     exit = Widget(Vector2Float(444, 4), Vector2Float(32, 32), None, Texture.EXIT, None, close)
 
-    showquote()
+    showQuote()
 
     def onclick():
-        showquote()
+        showQuote()
         jendrok.spin()
 
     root = Widget(Vector2Float(0, 0), Vector2Float(WIDTH, HEIGHT), Color(255, 255, 255), None, None, onclick)
@@ -113,7 +116,7 @@ def main():
                 pos = pygame.mouse.get_pos()
                 clickPosition = Vector2Float(pos[0], pos[1])
 
-        deltaTime = clock.tick(60) / 1000.0
+        deltaTime = clock.tick(16) / 1000.0
         root.animate(deltaTime)
         root.click(clickPosition)
 
